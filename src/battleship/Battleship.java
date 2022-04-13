52518348b17f7a58415a24a453e17c3574723f25
amalofeev <9151532547@mail.ru>
2022-04-13 14:35:49 +0300

    Restructurised Battlefield class, added input data validation

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -7,23 +7,35 @@ import java.util.HashMap;
 
 public class Battleship {
 
+    private static char leftCharCoord, rigthCharCoord;
+    private static int leftIntCoord, rigthIntCoord;
+
     private static HashMap<Character, String[]> battleField = new HashMap<>();
+    private static ArrayList<Object []> createdShipsCoordinates = new ArrayList<>();
+    private static Object[][] battleShips = new Object[][]{
+            {"Aircraft Carrier", 5},
+            {"Battleship", 4},
+            {"Submarine", 3},
+            {"Cruiser", 3},
+            {"Destroyer", 2}
+    };
 
     public static void main(String[] args) {
-
-        // create HashMap battleField, where 'i' is a character from 'A' to 'J'(inclusively)
-        for (int i=65; i<75; i++) {
-            battleField.put((char) i, new String[] {" ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~"});
-        }
-
-        printBattelField(battleField);
-        takeCoordinates();
-
+        createBattleField(battleField);
+        printBattleField(battleField);
+        fillBattleField(createdShipsCoordinates, battleShips);
     }
 
 
-    public static void printBattelField(HashMap<Character, String[]> battleField) {
+    public static void createBattleField(HashMap<Character, String[]> battleField) {
+        // create a HashMap "battleField", where 'i' is a character from 'A' to 'J'(inclusively)
+        for (int i=(int) 'A'; i<=(int) 'J'; i++) {
+            battleField.put((char) i, new String[] {" ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~"});
+        }
+    }
 
+
+    public static void printBattleField(HashMap<Character, String[]> battleField) {
         System.out.println("  1 2 3 4 5 6 7 8 9 10");
 
         for (Character key: battleField.keySet()) {
@@ -32,17 +44,42 @@ public class Battleship {
     }
 
 
-    public static boolean compareCoordinates(
-            ArrayList<Object []> createdShipsCoordinates, char leftCharCoord, int leftIntCoord, char rigthCharCoord, int rigthIntCoord
-    ) {
+    public static boolean takeCoordinates() {
+        String[] coordinates;
+        boolean flag = false;
 
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            coordinates = sc.nextLine().toUpperCase().split(" ");
+            try {
+                leftCharCoord = coordinates[0].charAt(0);
+                leftIntCoord = Integer.parseInt(coordinates[0].substring(1));
+                rigthCharCoord = coordinates[1].charAt(0);
+                rigthIntCoord = Integer.parseInt(coordinates[1].substring(1));
+            } catch (IllegalArgumentException e) {
+                flag = true;
+            }
+        } catch (IndexOutOfBoundsException e) {
+            flag = true;
+        }
+        return flag;
+    }
+
+
+    public static boolean compareCoordinates(
+            ArrayList<Object []> createdShipsCoordinates,
+            char leftCharCoord,
+            int leftIntCoord,
+            char rigthCharCoord,
+            int rigthIntCoord
+    ) {
         boolean answer = false;
         boolean compareIntCoord, compareCharCoord;
         int maxIntBoarder, minIntBoarder;
         int maxCharBoarder, minCharBoarder;
 
         for (Object[] shipsCoordinate : createdShipsCoordinates) {
-
             maxIntBoarder = Math.max((Integer) shipsCoordinate[1], (Integer) shipsCoordinate[3]) + 1;
             minIntBoarder = Math.min((Integer) shipsCoordinate[1], (Integer) shipsCoordinate[3]) - 1;
             compareIntCoord = (
@@ -68,54 +105,36 @@ public class Battleship {
     }
 
 
-    public static void takeCoordinates() {
-
-        Scanner sc = new Scanner(System.in);
-
-        char leftCharCoord, rigthCharCoord;
-        int leftIntCoord, rigthIntCoord;
-
-        String[] coordinates;
-
-        ArrayList<Object []> createdShipsCoordinates = new ArrayList<>();
-
-        Object[][] battleShips = new Object[][]{
-                {"Aircraft Carrier", 5},
-                {"Battleship", 4},
-                {"Submarine", 3},
-                {"Cruiser", 3},
-                {"Destroyer", 2}
-        };
+    public static void fillBattleField(ArrayList<Object []> createdShipsCoordinates, Object[][] battleShips) {
 
         for (Object[] battleShip : battleShips) {
             while (true) {
                 System.out.printf("Enter the coordinates of the %s (%d cells): \n\n", battleShip[0], battleShip[1]);
 
-                coordinates = sc.nextLine().split(" ");
-
-                leftCharCoord = coordinates[0].charAt(0);
-                leftIntCoord = Integer.parseInt(coordinates[0].substring(1));
-                rigthCharCoord = coordinates[1].charAt(0);
-                rigthIntCoord = Integer.parseInt(coordinates[1].substring(1));
+                // проверка входящих данных
+                if (takeCoordinates()) {
+                    System.out.println("Error! Wrong ship coordinates! Try again:");
+                    continue;
+                }
 
                 int lengthOfTheShip = Math.abs(leftIntCoord - rigthIntCoord) + 1;
 
-                // обработать случай, когда числа равны, а буквы - нет
-                if (lengthOfTheShip > (int) battleShip[1] || lengthOfTheShip < 0) {
-                    // Error! Wrong length of the Submarine! Try again: -- check lenght of the ship
+                if (lengthOfTheShip > (int) battleShip[1] || lengthOfTheShip < 1) {
                     System.out.printf("Error! Wrong length of the %s! Try again: \n\n", battleShip[0]);
-
-                    // проверить срабатывание условия - НЕ ОБРАБАТЫВАЕТ Г-ОБРАЗНЫЙ КОРАБЛЬ
-                } else if (((int) leftCharCoord + (int) rigthCharCoord) < (int) 'A' + (int) 'A' || ((int) leftCharCoord + (int) rigthCharCoord) > (int) 'J' + (int) 'J' || (leftIntCoord + rigthIntCoord) < 2 || (leftIntCoord + rigthIntCoord) > 20) {
-                    // Error! Wrong ship location! Try again: -- check boarders of battlefield
+                } else if (
+                    // проверка - если корабль не в одну линию
+                    (((int) leftCharCoord != (int) rigthCharCoord) && (leftIntCoord != rigthIntCoord)) ||
+                    // проверка границы поля боя
+                    ((int) leftCharCoord + (int) rigthCharCoord) < (int) 'A' + (int) 'A' ||
+                    ((int) leftCharCoord + (int) rigthCharCoord) > (int) 'J' + (int) 'J' ||
+                    (leftIntCoord + rigthIntCoord) < 2 ||
+                    (leftIntCoord + rigthIntCoord) > 20
+                ) {
                     System.out.println("Error! Wrong ship location! Try again:");
-
-                    // проверить работу функции
                 } else if (compareCoordinates(createdShipsCoordinates, leftCharCoord, leftIntCoord, rigthCharCoord, rigthIntCoord)) {
-                    // Error! You placed it too close to another one. Try again: -- check the position of the ship, before write down the " O"
                     System.out.println("Error! You placed it too close to another one. Try again:");
                 } else {
-                    for (int i=(int) leftCharCoord; i<=(int) rigthCharCoord; i++) {
+                    for (int i=Math.min((int) leftCharCoord, (int) rigthCharCoord); i<=Math.max((int) leftCharCoord, (int) rigthCharCoord); i++) {
                         for (int j=Math.min(leftIntCoord, rigthIntCoord); j<=Math.max(leftIntCoord, rigthIntCoord); j++) {
                             battleField.get((char) i)[j-1] = " O";
                         }
@@ -124,15 +143,7 @@ public class Battleship {
                     break;
                 }
             }
-            printBattelField(battleField);
+            printBattleField(battleField);
         }
     }
 }
-
-/*
-F3 F7
-A1 D1
-J10 J8
-
-J5 J7
-*/
