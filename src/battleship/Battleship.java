88759aa62bad15b6e195a9e52108d5c609d67529
amalofeev <9151532547@mail.ru>
2022-04-14 18:41:21 +0300

    Restucturised Battlefield method, added takeAshoot method

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -21,13 +21,14 @@ public class Battleship {
     };
 
     public static void main(String[] args) {
-        createBattleField(battleField);
-        printBattleField(battleField);
-        fillBattleField(createdShipsCoordinates, battleShips);
+        createBattleField();
+        printBattleField();
+        fillBattleField();
+        takeAshoot();
     }
 
 
-    public static void createBattleField(HashMap<Character, String[]> battleField) {
+    public static void createBattleField() {
         // create a HashMap "battleField", where 'i' is a character from 'A' to 'J'(inclusively)
         for (int i=(int) 'A'; i<=(int) 'J'; i++) {
             battleField.put((char) i, new String[] {" ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~", " ~"});
@@ -35,7 +36,7 @@ public class Battleship {
     }
 
 
-    public static void printBattleField(HashMap<Character, String[]> battleField) {
+    public static void printBattleField() {
         System.out.println("  1 2 3 4 5 6 7 8 9 10");
 
         for (Character key: battleField.keySet()) {
@@ -46,6 +47,7 @@ public class Battleship {
 
 
     public static boolean takeCoordinates() {
+        // input data validation
         String[] coordinates;
         boolean flag = false;
 
@@ -54,13 +56,22 @@ public class Battleship {
         try {
             coordinates = sc.nextLine().toUpperCase().split(" ");
             System.out.println();
-            try {
-                leftCharCoord = coordinates[0].charAt(0);
-                leftIntCoord = Integer.parseInt(coordinates[0].substring(1));
-                rigthCharCoord = coordinates[1].charAt(0);
-                rigthIntCoord = Integer.parseInt(coordinates[1].substring(1));
-            } catch (IllegalArgumentException e) {
-                flag = true;
+            if (coordinates.length == 1) {
+                try {
+                    leftCharCoord = coordinates[0].charAt(0);
+                    leftIntCoord = Integer.parseInt(coordinates[0].substring(1));
+                } catch (IllegalArgumentException e) {
+                    flag = true;
+                }
+            } else {
+                try {
+                    leftCharCoord = coordinates[0].charAt(0);
+                    leftIntCoord = Integer.parseInt(coordinates[0].substring(1));
+                    rigthCharCoord = coordinates[1].charAt(0);
+                    rigthIntCoord = Integer.parseInt(coordinates[1].substring(1));
+                } catch (IllegalArgumentException e) {
+                    flag = true;
+                }
             }
         } catch (IndexOutOfBoundsException e) {
             flag = true;
@@ -69,12 +80,35 @@ public class Battleship {
     }
 
 
+    public static boolean checkLengthOfTheShip(Object[] battleShip) {
+        int lengthOfTheShip = (
+            (int) leftCharCoord == (int) rigthCharCoord ?
+                    Math.abs(leftIntCoord - rigthIntCoord) + 1 : Math.abs((int) leftCharCoord - (int) rigthCharCoord) + 1
+        );
+        return lengthOfTheShip != (int) battleShip[1];
+    }
+
+
+    public static boolean checkShapeOfTheShip() {
+        // validation if the ship is not in one line
+        return ((int) leftCharCoord != (int) rigthCharCoord) && (leftIntCoord != rigthIntCoord);
+    }
+
+    public static boolean checkBattlefieldBoundaries() {
+        // validation of the battlefield boundaries
+        return ((int) leftCharCoord * (int) rigthCharCoord) < (int) 'A' * (int) 'A' ||
+                ((int) leftCharCoord * (int) rigthCharCoord) > (int) 'J' * (int) 'J' ||
+                (leftIntCoord * rigthIntCoord) < 1 ||
+                (leftIntCoord * rigthIntCoord) > 100;
+    }
+
+
     public static boolean compareCoordinates(
-            ArrayList<Object []> createdShipsCoordinates,
             char leftCharCoord,
             int leftIntCoord,
             char rigthCharCoord,
-            int rigthIntCoord
+            int rigthIntCoord,
+            int additionalBoundary
     ) {
         boolean answer = false;
         boolean compareIntCoord, compareCharCoord;
@@ -82,16 +116,16 @@ public class Battleship {
         int maxCharBoarder, minCharBoarder;
 
         for (Object[] shipsCoordinate : createdShipsCoordinates) {
-            maxIntBoarder = Math.max((Integer) shipsCoordinate[1], (Integer) shipsCoordinate[3]) + 1;
-            minIntBoarder = Math.min((Integer) shipsCoordinate[1], (Integer) shipsCoordinate[3]) - 1;
+            maxIntBoarder = Math.max((Integer) shipsCoordinate[1], (Integer) shipsCoordinate[3]) + additionalBoundary;
+            minIntBoarder = Math.min((Integer) shipsCoordinate[1], (Integer) shipsCoordinate[3]) - additionalBoundary;
             compareIntCoord = (
                     leftIntCoord >= minIntBoarder && leftIntCoord <= maxIntBoarder
             ) || (
                     rigthIntCoord >= minIntBoarder && rigthIntCoord <= maxIntBoarder
             );
 
-            maxCharBoarder = Math.max((int) (char) shipsCoordinate[0], (int) (char) shipsCoordinate[2]) + 1;
-            minCharBoarder = Math.min((int) (char) shipsCoordinate[0], (int) (char) shipsCoordinate[2]) - 1;
+            maxCharBoarder = Math.max((int) (char) shipsCoordinate[0], (int) (char) shipsCoordinate[2]) + additionalBoundary;
+            minCharBoarder = Math.min((int) (char) shipsCoordinate[0], (int) (char) shipsCoordinate[2]) - additionalBoundary;
             compareCharCoord = (
                     (int) leftCharCoord >= minCharBoarder && (int) leftCharCoord <= maxCharBoarder
             ) || (
@@ -107,34 +141,34 @@ public class Battleship {
     }
 
 
-    public static void fillBattleField(ArrayList<Object []> createdShipsCoordinates, Object[][] battleShips) {
+    public static boolean validator(Object[] battleShip) {
+        boolean flag = false;
 
+        if (takeCoordinates()) {
+            flag = true;
+            System.out.println("Error! Wrong ship coordinates! Try again:\n");
+        } else if (checkLengthOfTheShip(battleShip)) {
+            flag = true;
+            System.out.printf("Error! Wrong length of the %s! Try again: \n\n", battleShip[0]);
+        } else if (
+            checkShapeOfTheShip() || checkBattlefieldBoundaries()
+        ) {
+            flag = true;
+            System.out.println("Error! Wrong ship location! Try again:\n");
+        } else if (compareCoordinates(leftCharCoord, leftIntCoord, rigthCharCoord, rigthIntCoord, 1)) {
+            flag = true;
+            System.out.println("Error! You placed it too close to another one. Try again:\n");
+        }
+
+        return flag;
+    }
+
+
+    public static void fillBattleField() {
         for (Object[] battleShip : battleShips) {
             System.out.printf("Enter the coordinates of the %s (%d cells): \n\n", battleShip[0], battleShip[1]);
             while (true) {
-                // input data validation
-                if (takeCoordinates()) {
-                    System.out.println("Error! Wrong ship coordinates! Try again:\n");
-                    continue;
-                }
-
-                int lengthOfTheShip = (((int) leftCharCoord == (int) rigthCharCoord) ? Math.abs(leftIntCoord - rigthIntCoord) + 1 : Math.abs((int) leftCharCoord - (int) rigthCharCoord) + 1);
-
-                if (lengthOfTheShip != (int) battleShip[1]) {
-                    System.out.printf("Error! Wrong length of the %s! Try again: \n\n", battleShip[0]);
-                } else if (
-                    // validation if the ship is not in one line
-                    (((int) leftCharCoord != (int) rigthCharCoord) && (leftIntCoord != rigthIntCoord)) ||
-                    // validation of the battlefield boundaries
-                    ((int) leftCharCoord * (int) rigthCharCoord) < (int) 'A' * (int) 'A' ||
-                    ((int) leftCharCoord * (int) rigthCharCoord) > (int) 'J' * (int) 'J' ||
-                    (leftIntCoord * rigthIntCoord) < 1 ||
-                    (leftIntCoord * rigthIntCoord) > 100
-                ) {
-                    System.out.println("Error! Wrong ship location! Try again:\n");
-                } else if (compareCoordinates(createdShipsCoordinates, leftCharCoord, leftIntCoord, rigthCharCoord, rigthIntCoord)) {
-                    System.out.println("Error! You placed it too close to another one. Try again:\n");
-                } else {
+                if (!validator(battleShip)) {
                     for (int i=Math.min((int) leftCharCoord, (int) rigthCharCoord); i<=Math.max((int) leftCharCoord, (int) rigthCharCoord); i++) {
                         for (int j=Math.min(leftIntCoord, rigthIntCoord); j<=Math.max(leftIntCoord, rigthIntCoord); j++) {
                             battleField.get((char) i)[j-1] = " O";
@@ -144,7 +178,35 @@ public class Battleship {
                     break;
                 }
             }
-            printBattleField(battleField);
+            printBattleField();
+        }
+    }
+
+    public static void takeAshoot() {
+        System.out.println("The game starts!\n");
+        printBattleField();
+        System.out.println("Take a shot!\n");
+        while (true) {
+            if (takeCoordinates() || (
+                    (int) leftCharCoord < (int) 'A' ||
+                    (int) leftCharCoord > (int) 'J' ||
+                    leftIntCoord < 1 ||
+                    leftIntCoord > 10
+                )
+            ) {
+                System.out.println("Error! You entered the wrong coordinates! Try again:\n");
+                continue;
+            }
+            if (compareCoordinates(leftCharCoord, leftIntCoord, '!', 0, 0)) {
+                battleField.get(leftCharCoord)[leftIntCoord-1] = " X";
+                printBattleField();
+                System.out.println("You hit a ship!\n");
+            } else {
+                battleField.get(leftCharCoord)[leftIntCoord-1] = " M";
+                printBattleField();
+                System.out.println("You missed!\n");
+            }
+            break;
         }
     }
 }
